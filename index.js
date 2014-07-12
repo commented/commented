6fe966c132f4de2986cf217380b63c1b6981756e
Jared Forsyth <jared@khanacademy.org>
2014-07-11 22:15:19 -0700

    better default slug

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 
 var Commented = require('./lib')
 
+function defaultSlug() {
+    return (window.location.host + window.location.pathname);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   var node = document.getElementById('commented-main');
   if (!node) {
-    console.error('#commented node not found');
+    console.error('#commented-main node not found');
     return;
   }
   var options = {
     firebase: node.getAttribute('data-firebase'),
     type: 'firebase',
     auth: node.getAttribute('data-auth').split(' '),
-    slug: node.getAttribute('data-slug') || window.location + '',
+    slug: node.getAttribute('data-slug') || defaultSlug(),
     inline: node.getAttribute('data-inline'),
     main: node
   }
